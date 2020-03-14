c6ffe4e08a70b6156e25f2dbe74140d15d06086e
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-14 14:53:47 +0200

    Bug fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
   } else if (pathname === '/about') {
-    res.writeHead(200, {'Content-Type': 'text/html'{});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(about);
   } else if (pathname === '/contact') {
     res.writeHead(200, {'Content-Type': 'text/html'});
