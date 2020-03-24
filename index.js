3a792888a81aad51a99e0de258d34a49913867b3
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-24 17:54:01 +0200

    Convert to express

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-const filePath = path.join(__dirname, '/pages');
-const index = fs.readFileSync(`${filePath}/index.html`).toString();
-const about = fs.readFileSync(`${filePath}/about.html`).toString();
-const contact = fs.readFileSync(`${filePath}/contact.html`).toString();
-const errorPage = fs.readFileSync(`${filePath}/404.html`).toString();
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const pathname = url.parse(req.url).pathname;
-
-  if (pathname === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-  } else if (pathname === '/about') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(about);
-  } else if (pathname === '/contact') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(contact);
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end(errorPage);
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
-server.listen(8080);
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'contact.html'));
+});
+
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'pages', '404.html'));
+});
+
+
+
+app.listen(8080);
